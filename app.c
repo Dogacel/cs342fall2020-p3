f13105f57bbffe1983739935b3b9fad604b14e4b
Ibrahim Korpeoglu <korpe@cs.bilkent.edu.tr>
2020-11-27 17:35:31 +0300

    initial

---
+++ app.c
@@ -0,0 +1,17 @@
+
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "smemlib.h"
+
+int main()
+{
+
+
+    mem_create_segment(); 
+    
+
+
+    return (0); 
+}
---
+++ smemlib.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "smemlib.h"
+
+
+int mem_create_segment()
+{
+    printf ("this is a function\n"); 
+    return (0); 
+}
